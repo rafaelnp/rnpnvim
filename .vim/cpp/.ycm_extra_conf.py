370ed9c38a6fb2141ba58f181d2518f0928d21e1
Rafael do Nascimento Pereira <rnp@25ghz.net>
2014-05-20 09:48:52 +0200

    Extended YCM C++ syntatic completion file
    Change-Id: Ic5a46cb35febfbe332310ece30111b2876099612

--- .vim/cpp/.ycm_extra_conf.py
+++ .vim/cpp/.ycm_extra_conf.py
@@ -1,6 +1,7 @@
 
 import os
 import ycm_core
+from clang_helpers import PrepareClangFlags
 
 # These are the compilation flags that will be used in case there's no
 # compilation database set (by default, one is not set).
@@ -8,57 +9,59 @@ import ycm_core
 flags = [
 '-Wall',
 '-Wextra',
+'-fPIE'
 #'-Wc++98-compat',
 '-Wno-long-long',
 '-Wno-variadic-macros',
 '-fexceptions',
 '-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
-# source code needs it.
-'-DUSE_CLANG_COMPLETER',
+'-D_REENTRANT',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
 # a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
-#'-std=c99',
 '-std=c++11',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-#'c',
-'c++',
-'-I',
-'.',
-'-isystem',
-'/usr/lib/c++/v1',
-'-I',
-'/usr/include',
-'-I',
-'/usr/include/arpa',
-'-I',
-'/usr/include/bits',
-'-I',
-'/usr/include/x86_64-linux-gnu',
-'-I',
-'/usr/include/linux',
-'-I',
-'/usr/include/net',
-'-I',
-'/usr/include/netinet',
-'-I',
-'/usr/include/pci',
-'-I',
-'/usr/include/proc',
-'-I',
-'/usr/include/sys',
-'-I',
-'/usr/include/linux',
-'-I',
-'/usr/include/asm-generic'
+'-x', 'c++',
+'-stdlib=libc++',
+'-I', '.',
+'-I', '/usr/include/c++',
+'-I', '/usr/include',
+'-I', '/usr/include/arpa',
+'-I', '/usr/include/bits',
+'-I', '/usr/include/x86_64-linux-gnu',
+'-I', '/usr/include/linux',
+'-I', '/usr/include/net',
+'-I', '/usr/include/netinet',
+'-I', '/usr/include/pci',
+'-I', '/usr/include/proc',
+'-I', '/usr/include/sys',
+'-I', '/usr/include/linux',
+'-I', '/usr/include/asm-generic'
+'-I', '/usr/include/qt',
+'-I', '/usr/include/qt/Qt',
+'-I', '/usr/include/qt/QtConcurrent',
+'-I', '/usr/include/qt/QtCore',
+'-I', '/usr/include/qt/QtDBus',
+'-I', '/usr/include/qt/QtGui',
+'-I', '/usr/include/qt/QtHelp',
+'-I', '/usr/include/qt/QtMultimedia',
+'-I', '/usr/include/qt/QtMultimediaWidgets',
+'-I', '/usr/include/qt/QtNetwork',
+'-I', '/usr/include/qt/QtOpenGL',
+'-I', '/usr/include/qt/QtPlatformSupport',
+'-I', '/usr/include/qt/QtPositioning',
+'-I', '/usr/include/qt/QtScript',
+'-I', '/usr/include/qt/QtScriptTools',
+'-I', '/usr/include/qt/QtSql',
+'-I', '/usr/include/qt/QtSvg',
+'-I', '/usr/include/qt/QtTest',
+'-I', '/usr/include/qt/QtUiTools',
+'-I', '/usr/include/qt/QtV8',
+'-I', '/usr/include/qt/QtWebKit',
+'-I', '/usr/include/qt/QtWebKitWidgets',
+'-I', '/usr/include/qt/QtWidgets',
+'-I', '/usr/include/qt/QtXml',
+'-I', '/usr/include/qt/QtXmlPatterns',
 ]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
@@ -73,7 +76,6 @@ if compilation_database_folder:
 	database = ycm_core.CompilationDatabase( compilation_database_folder )
 else:
 	database = None
-
 
 def DirectoryOfThisScript():
 	return os.path.dirname( os.path.abspath( __file__ ) )
